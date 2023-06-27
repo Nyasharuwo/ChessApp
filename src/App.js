b49586448503a17847c2bd26a431571541ab5aba
Nyasharuwo <122794943+Nyasharuwo@users.noreply.github.com>
2023-06-27 13:12:54 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,29 @@ import { useState } from "react";
 import "./App.css";
 import Board from "./components/Board/Board";
 import { INITIAL_CHESS_BOARD } from "./utils/consts";
+//import io from "socket.io-client";
+//import Chat from "./Chat";
 
 function App() {
   const [piecesOnBoard, setPiecesOnBoard] = useState(INITIAL_CHESS_BOARD);
+ //const [currentPlayer, setCurrentPlayer] = useState("white");
+ // const [chatMessages, setChatMessages] = useState([]);
+ //  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+   
+    socket.on("board", (updatedBoard) => {
+      setBoard(updatedBoard);
+    });
+
+    socket.on("playerTurn", (player) => {
+      setCurrentPlayer(player);
+    });
+
+    socket.on("chatMessage", (message) => {
+      setChatMessages((prevMessages) => [...prevMessages, message]);
+    });
+    
   return (
     <div className="App">
       <Board
